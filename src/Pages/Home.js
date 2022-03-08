6c75b042042f8c58fe75f74a9bb36e8d0fadb244
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-08 16:03:48 +0100

    nav imported

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Nav from "./../Components/Nav";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Nav />
+      Home
+    </div>
+  );
 }
