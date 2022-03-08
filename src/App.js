09d35c06536185bc5e54fd68da84cc792df3781a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-08 07:51:58 +0100

    icons-material i

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <AccessAlarmIcon />
+      Hello, world!
+    </div>
+  );
 }
 
 export default App;
