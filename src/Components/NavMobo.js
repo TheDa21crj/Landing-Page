18cc82b27d61c2629aae1296652de94862e32754
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-08 16:37:03 +0100

    creating mobo nav

--- src/Components/NavMobo.js
+++ src/Components/NavMobo.js
@@ -1,7 +1,20 @@
 import React from "react";
+import imgLogo from "./../Img/LogoImg.png";
 import { Link } from "react-router-dom";
 import "./Css/NavMobo.css";
 
 export default function NavMobo() {
-  return <div>NavMobo</div>;
+  return (
+    <div className="NavMoboMDiv">
+      <img src={imgLogo} alt="" />
+      <button className="NavLoginbtn">
+        <p>Login</p>
+      </button>
+      <div className="hamBurger">
+        <div className="ham1"></div>
+        <div className="ham2"></div>
+        <div className="ham3"></div>
+      </div>
+    </div>
+  );
 }
