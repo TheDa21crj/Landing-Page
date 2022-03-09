b04f5f41ca5052f81a8836528133972bd828f769
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-09 15:10:24 +0100

    header h1 styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Css/Header.css";
 
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className="HeaderMDiv">
+      <div className="HeaderH1Div">
         <h1>Work at the speed of thought</h1>
       </div>
     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./../Components/Nav";
 import NavMobo from "./../Components/NavMobo";
+import Header from "./../Components/Header";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <NavMobo />
+      <Header />
       Home
     </div>
   );
