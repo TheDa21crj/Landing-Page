366ea0998b922a64c8db74437e32802e2360f5c1
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-09 16:56:50 +0100

    tags added

---
+++ src/Components/Features.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Css/Features.css";
+
+export default function Features() {
+  return (
+    <div className="featuresmDiv">
+      <h1>Features</h1>
+      <div>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
