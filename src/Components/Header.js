42d074bfbeafdc538c8ec08683062ebb045510d7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-09 15:15:17 +0100

    p tag styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,12 @@ export default function Header() {
       <div className="HeaderH1Div">
         <h1>Work at the speed of thought</h1>
       </div>
+      <div className="HeaderPDiv">
+        <p>
+          Most calendars are designed for teams. Slate is designed for
+          freelancers who want a simple way to plan their schedule.
+        </p>
+      </div>
     </div>
   );
 }
