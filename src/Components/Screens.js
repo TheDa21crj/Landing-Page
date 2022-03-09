d05b9d515e79674e3477c5c95a47b40ee386fa1b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-09 15:44:16 +0100

    img added

--- src/Components/Screens.js
+++ src/Components/Screens.js
@@ -1,5 +1,12 @@
 import React from "react";
+import imgScreen from "./../Img/screens.png";
 
 export default function Screens() {
-  return <div>Screens</div>;
+  return (
+    <div>
+      <div>
+        <img src={imgScreen} alt="" className="screensImg" />
+      </div>
+    </div>
+  );
 }
