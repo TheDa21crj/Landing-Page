ff93aa326a1780600802262907071f9d125c6806
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-09 17:35:08 +0100

    Update NavMobo.js

--- src/Components/NavMobo.js
+++ src/Components/NavMobo.js
@@ -4,17 +4,22 @@ import { Link } from "react-router-dom";
 import "./Css/NavMobo.css";
 
 export default function NavMobo() {
-  return (
-    <div className="NavMoboMDiv">
-      <img src={imgLogo} alt="" />
-      <button className="NavLoginbtn">
-        <p>Login</p>
-      </button>
-      <div className="hamBurger">
-        <div className="ham1"></div>
-        <div className="ham2"></div>
-        <div className="ham3"></div>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "NavMoboMDiv" >
+        <
+        img src = { imgLogo }
+        alt = "" / >
+        <
+        button className = "NavLoginbtn" >
+        <
+        p > Login < /p> <
+        /button> <
+        div className = "hamBurger" >
+        <
+        div className = "ham1" > < /div> <
+        div className = "ham2" > < /div> <
+        div className = "ham3" > < /div> <
+        /div> <
+        /div>
+    );
 }
