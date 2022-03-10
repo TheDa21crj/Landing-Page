bdeb40f67a41aff2510661c290f77cbd72eb58a2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-10 23:28:52 +0100

    i Gallery

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ import Header from "./../Components/Header";
 import Screens from "./../Components/Screens";
 import Features from "./../Components/Features";
 import Contents from "./../Components/Contents";
+import Gallery from "./../Components/Gallery";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Screens />
       <Features />
       <Contents />
+      <Gallery />
       Home
     </div>
   );
