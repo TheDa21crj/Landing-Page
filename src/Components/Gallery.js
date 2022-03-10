9b4d66e39ac4fff5728c002c301bb1d8f6773554
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-10 23:29:27 +0100

    Update Gallery.js

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Gallery() {
-  return <div>Gallery</div>;
+  return (
+    <div>
+      <p className="GalleryH1">Gallery</p>
+    </div>
+  );
 }
