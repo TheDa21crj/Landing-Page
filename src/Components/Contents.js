177e95ccc37b5b0e796f0746ee7b0d1d2f35ab08
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-10 17:17:10 +0100

    subheading added

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -5,6 +5,15 @@ export default function Contents() {
   return (
     <div className="ContentsMDiv">
       <p className="ContentsHeadPTag">Contents</p>
+      <div>
+        <div>
+          <p>
+            We focus on ergonomics and meeting you where you work. It's only a
+            keystroke away.
+          </p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
