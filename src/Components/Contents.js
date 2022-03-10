54439e1dcecb3f590c08e5417c8454e2c77bcc70
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-10 17:50:17 +0100

    Update Contents.js

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -14,10 +14,14 @@ export default function Contents() {
         </div>
         <div className="ContentsRowDiv">
           <div className="ContentLefthtDiv">
+            {/* <div> */}
             <p className="CardsDivPTag">Work</p>
+            {/* </div> */}
           </div>
           <div className="ContentRightDiv">
+            {/* <div> */}
             <p className="CardsDivPTag">Design with real data</p>
+            {/* </div> */}
           </div>
         </div>
       </div>
