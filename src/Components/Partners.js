15ccf6e149404ce8985d558b2af481585d05f35f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-12 15:47:10 +0100

    content added

--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -5,6 +5,12 @@ export default function Partners() {
   return (
     <div>
       <p className="PartnersmDiv">Partners</p>
+      <div className="PartnersContentDiv">
+        <p>
+          We focus on ergonomics and meeting you where you work. It's only a
+          keystroke away.
+        </p>
+      </div>
     </div>
   );
 }
