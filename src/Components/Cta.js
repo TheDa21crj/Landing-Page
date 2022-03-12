5745ffc04f67741c0e007a44e6addf188e68271e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-12 20:38:11 +0100

    working for setup

--- src/Components/Cta.js
+++ src/Components/Cta.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Css/Cta.css";
 
 export default function Cta() {
-  return <div className="CtamDiv">Cta</div>;
+  return (
+    <div className="CtamDiv">
+      <div>hello</div>
+    </div>
+  );
 }
