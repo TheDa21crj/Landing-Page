05ca6d366c5817155d3de85c658e208c64ecca09
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-12 20:43:30 +0100

    setting up

--- src/Components/Cta.js
+++ src/Components/Cta.js
@@ -4,7 +4,10 @@ import "./Css/Cta.css";
 export default function Cta() {
   return (
     <div className="CtamDiv">
-      <div className="CtaContentDiv">hello</div>
+      <div className="CtaContentDiv">
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
