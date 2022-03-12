eec93f6d01c71364b3acbbc5fdd23dd18f410175
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-12 22:35:05 +0100

    setting up

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,5 +2,24 @@ import React from "react";
 import "./Css/Footer.css";
 
 export default function Footer() {
-  return <div className="FooterMDiv">Footer</div>;
+  return (
+    <div className="FooterMDiv">
+      <div>
+        <div>
+          <div>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
