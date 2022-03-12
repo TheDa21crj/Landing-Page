57beb47dbb8acaa1d2ddb88db4ec21835438c7d6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-03-12 14:36:22 +0100

    className

--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Css/Partners.css";
 
 export default function Partners() {
-  return <div>Partners</div>;
+  return (
+    <div>
+      <p className="PartnersmDiv">Partners</p>
+    </div>
+  );
 }
